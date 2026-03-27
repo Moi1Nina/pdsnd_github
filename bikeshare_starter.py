0808628faf1de89ffdb3bb41c4b5cf28eaa6f734
Nina <nina.cesur@cariad.technology>
2026-03-27 14:48:21 +0100

    Add comment1

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -17,6 +17,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # added this comment1
 
 
     # get user input for month (all, january, february, ... , june)
